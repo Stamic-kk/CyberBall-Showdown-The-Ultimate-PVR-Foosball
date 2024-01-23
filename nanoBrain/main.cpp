a536fc3c05388442c54cb4483a7939d2853dbd20
Linus <li3975@purdue.edu>
2024-01-23 18:52:40 -0500

    add cv output

--- nanoBrain/main.cpp
+++ nanoBrain/main.cpp
@@ -20,14 +20,32 @@
 #include <iostream>
 #include <sstream>
 #include <time.h>
+#include <vector>
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+using std::string;
+using std::vector;
 
+vector<string> getAllFiles(string path){
+    vector<string> files;
+    for(const auto & entry : fs::directory_iterator(path)){
+        files.push_back(entry.path());
+    }
+    for(string file : files){
+        std::cout<<file<<std::endl;
+    }
+    return files;
+}
 
 int main(int argc, char**argv){
+    std::cout<<CV_MAJOR_VERSION<<std::endl;
+    vector<string> files_path = getAllFiles("inputs/");
     time_t start, end;
-    cv::Mat cvImage = cv::imread(argv[1]);
+    for(string file_name : files_path){
+    cv::Mat cvImage = cv::imread(file_name);
     VPIImage inputBGR = NULL;
     VPIImage input = NULL;
-
+    cv::Mat outCvImage;
     VPIStream stream;
     vpiStreamCreate(0, &stream);
     start = clock();
@@ -83,15 +101,20 @@ int main(int argc, char**argv){
     unsigned char min_value = min_row[min_j];
     unsigned char max_value = max_row[max_j];
     std::cout << "min: " << (int)min_value << " at (" << min_i << ", " << min_j << ")" << std::endl;
+    vpiImageDataExportOpenCVMat(inputImageData, &outCvImage);
+    cv::circle(outCvImage, cv::Point(min_j, min_i), 100, cv::Scalar(0, 0, 255), 20);
+    cv::imwrite("outputs/"+file_name.substr(7), outCvImage);
     vpiArrayUnlock(maxCoords);
     vpiArrayUnlock(minCoords);
     vpiImageUnlock(input);
-
     vpiStreamDestroy(stream);
     vpiPayloadDestroy(payload);
     vpiImageDestroy(input);
     vpiArrayDestroy(minCoords);
     vpiArrayDestroy(maxCoords);
+       
+    } 
+
 
     return 0;
 
